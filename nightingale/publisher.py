9480a3ccf7f87747305114ba2bb8c3ac11482f29
yshalenyk <yshalenyk@gmail.com>
2024-06-14 13:26:59 +0300

    chore(docs): add docstrings to DataPublisher

--- nightingale/publisher.py
+++ nightingale/publisher.py
@@ -1,24 +1,49 @@
 from datetime import datetime
 from typing import Any
 
+from .utils import produce_package_name
+
 
 class DataPublisher:
-    """Packs array of releases into a release package"""
+    """
+    Packs array of releases into a release package.
+
+    :param config: Configuration object containing settings for the publisher.
+    :type config: Config
+    """
 
     def __init__(self, config):
+        """
+        Initialize the DataPublisher.
+
+        :param config: Configuration object containing settings for the publisher.
+        :type config: Config
+        """
         self.config = config
 
-    def produce_ocid(self, value):
-        prefix = self.config.ocid_prefix
-        return f"{prefix}-{value}"
+    def produce_uri(self, date: str) -> str:
+        """
+        Produce a URI for the package based on the given date.
 
-    def package(self, data: list[dict[str, Any]]):
+        :param date: The date to use for generating the URI.
+        :type date: str
+        :return: The produced URI.
+        :rtype: str
+        """
+        return f"{self.config.base_uri}/{produce_package_name(date)}"
+
+    def package(self, data: list[dict[str, Any]]) -> dict[str, Any]:
+        """
+        Package the given data into a release package.
+
+        :param data: List of release dictionaries to be packaged.
+        :type data: list[dict[str, Any]]
+        :return: A dictionary representing the release package.
+        :rtype: dict[str, Any]
+        """
         now = datetime.now().isoformat()
-        for r in data:
-            r["ocid"] = self.produce_ocid(r["ocid"])
-        # XXX: should it be now or other date?
         return {
-            "uri": f"https://todo.com/{now}",
+            "uri": self.produce_uri(now),
             "version": self.config.version,
             "publisher": self.config.publisher,
             "publishedDate": now,
